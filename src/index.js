5c7e38d53b5bb25596533c6db0fd140931fd212c
Omer Szerer <omerszerer@gmail.com>
2024-09-19 22:36:19 +0200

    Add a google font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Protest+Guerrilla&display=swap');
+</style>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
